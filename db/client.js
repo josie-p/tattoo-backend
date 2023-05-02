7766e60b959110b4d06d0009702efb086db1568a
Josie Parker <josieparker@Josies-MacBook-Pro.local>
2023-05-02 13:20:42 -0500

    set up db client

---
+++ db/client.js
@@ -0,0 +1,16 @@
+const DB_NAME = "jo_tattoos";
+
+const { Client } = require("pg"); //imports the pg module
+
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+//supply the db name and location of the db
+
+const client = new Client({
+    connectionString,
+    ssl: 
+    process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : undefined, 
+});
+
+module.exports = client;
